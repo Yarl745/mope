d041b18d9e378f7084adc5d488e7310b455afefc
Yarikslav <kopailo2002@gmail.com>
2021-05-24 15:03:15 +0200

    Add time for static check.

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -4,12 +4,11 @@ import pprint
 from scipy.stats import t, f
 import sklearn.linear_model as lm
 from functools import partial
-
-
+from time import time
 
 x_range = [[-6, 10], [-3, 5], [-4, 9]]
-x_sered_max = sum([x[1] for x in x_range])/3
-x_sered_min = sum([x[0] for x in x_range])/3
+x_sered_max = sum([x[1] for x in x_range]) / 3
+x_sered_min = sum([x[0] for x in x_range]) / 3
 
 x01 = (x_range[0][1] - x_range[0][0]) / 2
 x02 = (x_range[1][1] - x_range[1][0]) / 2
@@ -20,6 +19,7 @@ delta_x3 = x_range[2][1] - x03
 
 y_max = 200 + x_sered_max
 y_min = 200 + x_sered_min
+
 
 def create_plan_matrix(n, m):
     y = np.zeros(shape=(n, m))
@@ -75,8 +75,9 @@ def create_plan_matrix(n, m):
     y_avr = np.zeros(n)
     for i in range(len(y)):
         for j in range(len(y[0])):
-            y_avr[i] += y[i][j]/ m
+            y_avr[i] += y[i][j] / m
     return [x_matrix_norm, x_matrix, y, y_avr]
+
 
 def find_coefs(x, y):
     skm = lm.LinearRegression(fit_intercept=False)  # знаходимо коефіцієнти рівняння регресії
@@ -86,14 +87,16 @@ def find_coefs(x, y):
     print(B)
     return B
 
+
 def perevirka(x, y, b):
     y_pract = np.zeros(len(y))
     for i in range(len(x)):
         for j in range(len(x[0])):
             y_pract[i] += b[j] * x[i][j]
     print("\nПеревірка:")
-    print("\ny - real :" , y)
+    print("\ny - real :", y)
     print('\ny - found:', y_pract)
+
 
 
 def get_new_y(x, b):
@@ -103,38 +106,42 @@ def get_new_y(x, b):
             y_pract[i] += b[j] * x[i][j]
     return y_pract
 
+
 def get_cohren_critical(prob, f1, f2):
     f_crit = f.isf((1 - prob) / f2, f1, (f2 - 1) * f1)
     return f_crit / (f_crit + f2 - 1)
 
+
 def cohren_crit(y, n, m):
     y_var = [np.var(i) for i in y]
-    Gp = max(y_var)/sum(y_var)
-    Gt = get_cohren_critical(0.95, m-1, n)
-    if(Gp < Gt):
+    Gp = max(y_var) / sum(y_var)
+    Gt = get_cohren_critical(0.95, m - 1, n)
+    if (Gp < Gt):
         print("\nДисперсії однорідні")
         return True
     else:
         print("\nДисперсії не однорідні")
         return False
 
+
 fisher_teor = partial(f.ppf, q=1 - 0.05)
 
 student_teor = partial(t.ppf, q=1 - 0.025)
+
 
 def kriteriy_studenta(x, y, y_aver, n, m, B):
     d = 0
     y_var = [np.var(i) for i in y]
     s_kv_aver = sum(y_var) / n
-    s_aver = (s_kv_aver/(n * m))** 0.5
+    s_aver = (s_kv_aver / (n * m)) ** 0.5
     b = np.zeros(len(x[0]))
     for i in range(len(x[0])):
         for j in range(len(y_aver)):
             b[i] += y_aver[j] * x[j][i] / n
     ts = []
     for bi in b:
-        ts.append(bi/s_aver)
-    Stud_teor = student_teor(df = (m-1) * n)
+        ts.append(bi / s_aver)
+    Stud_teor = student_teor(df=(m - 1) * n)
     for i in range(len(ts)):
         if ts[i] < Stud_teor:
             B[i] = 0
@@ -144,17 +151,18 @@ def kriteriy_studenta(x, y, y_aver, n, m, B):
     print(B)
     return [B, d]
 
+
 def kriteriy_fishera(m, n, d, new_y_pract, y_avr, y):
     f4 = n - d
-    f3 = (m-1)*n
+    f3 = (m - 1) * n
     y_var = [np.var(i) for i in y]
-    Sa = (sum(y_var)/ n)
-    Sad = m/(n-d)* sum([(new_y_pract[i] - y_avr[i])**2 for i in range(len(y_avr))])
-    pract = Sad/Sa
-    teor = fisher_teor(dfn= f4, dfd=f3)
+    Sa = (sum(y_var) / n)
+    Sad = m / (n - d) * sum([(new_y_pract[i] - y_avr[i]) ** 2 for i in range(len(y_avr))])
+    pract = Sad / Sa
+    teor = fisher_teor(dfn=f4, dfd=f3)
     if pract > teor:
-        print("\nПрактичне значення:" , pract)
-        print("Теоретичне значення:" , teor)
+        print("\nПрактичне значення:", pract)
+        print("Теоретичне значення:", teor)
         print("\nРівняння регресії неадекватне")
         return [False, False]
     else:
@@ -162,26 +170,27 @@ def kriteriy_fishera(m, n, d, new_y_pract, y_avr, y):
         return [True, True]
 
 
-
-
 if __name__ == "__main__":
     odnorid = False
     adekvat = False
     n = 15
     m = 3
-    #поки не адекватно починаемо знову
+    # поки не адекватно починаемо знову
     while not adekvat:
-        #поки не однорідно починаемо знову
+        start = time()
+        # поки не однорідно починаемо знову
         while not odnorid:
             x_matrix_norm, x_matrix, y, y_avr = create_plan_matrix(n, m)
-            #однорідність перевіряется за критеріем кохрена
+            # однорідність перевіряется за критеріем кохрена
             odnorid = cohren_crit(y, n, m)
             # якщо не однорідне збільшуемо m і почиаемо знову
             if odnorid == False:
-                m+=1
-        B = find_coefs(x_matrix,    y_avr)
+                m += 1
+        B = find_coefs(x_matrix, y_avr)
         perevirka(x_matrix, y_avr, B)
         new_B, d = kriteriy_studenta(x_matrix_norm, y, y_avr, n, m, B)
         new_y_pract = get_new_y(x_matrix, new_B)
         # адекватність перевіряемо за критеріем фішера. приймае значення True або False
         adekvat, odnorid = kriteriy_fishera(m, n, 4, new_y_pract, y_avr, y)
+        end = time()
+        print(f'Час статичноi перевірки: {str(end - start)}\n\n\n')
