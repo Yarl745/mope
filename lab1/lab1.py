baf24a51106a950a702d3e92339a3a1b0ed03b22
Yarikslav <kopailo2002@gmail.com>
2021-05-27 19:49:40 +0200

    Modify lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,49 +1,53 @@
-from random import *
+import numpy as np
+from random import uniform
+import time
 
-a_0 = 4
-a_1 = 5
-a_2 = 8
-a_3 = 2
+MIN, MAX = 0, 20
+a0, a1, a2, a3 = 1, 2, 2, 3
 
-x_1 = sample(range(20), 8)
-x_2 = sample(range(20), 8)
-x_3 = sample(range(20), 8)
+X = np.empty((8, 3), dtype=float)  # X - масив значень факторів(усі точки X)
+Y = np.empty(8)  # Y -  масив значень функції відгуку (рівняння регресії Y = a0 + a1X1 + a2X2 + a3X3)
+X0 = np.empty(3)  # X0 - масив нульових рівнів для кожного фактору
+DX = np.empty(3)  # DX - масив інтервалів зміни фактора
+XNormalized = np.empty((8, 3), dtype=float)  # масив нормалізованих значень факторів
 
-Y = []
+startTime = time.time()
 for i in range(8):
-    Y.append(a_0 + a_1*x_1[i] + a_2*x_2[i] + a_3*x_3[i])
+    for j in range(3):
+        X[i, j] = uniform(MIN, MAX)
 
-x0_1 = (max(x_1) + min(x_1))/2
-x0_2 = (max(x_2) + min(x_2))/2
-x0_3 = (max(x_3) + min(x_3))/2
+endTime = time.time()
+print("--- %s seconds ---" % (endTime - startTime))
 
-dx_1 = x0_1 - min(x_1)
-dx_2 = x0_2 - min(x_2)
-dx_3 = x0_3 - min(x_3)
-
-xn_1 = []
 for i in range(8):
-    xn_1.append(round((x_1[i] - x0_1)/dx_1, 2))
+    Y[i] = a0 + a1 * X[i, 0] + a2 * X[i, 1] + a3 * X[i, 2]
 
-xn_2 = []
+for i in range(3):
+    X0[i] = (X[:, i].max() + X[:, i].min()) / 2
+    DX[i] = X[:, i].max() - X0[i]
+
+Y_et = a0 + a1 * X0[0] + a2 * X0[1] + a3 * X0[2]  # Y_et - Y-еталонне
 for i in range(8):
-    xn_2.append(round((x_2[i] - x0_2)/dx_2, 2))
+    for j in range(3):
+        XNormalized[i, j] = (X[i, j] - X0[j]) / DX[j]
 
-xn_3 = []
+dY = 999999  # різниця між Y таY_et
+number = -1  # номер точки, що щадовольняє критерій
+# знаходження номеру точки, що задовольняє критерій опримальності
+# Yэт
+# пошук точки в якій значення Y найближче справа до Y_et
 for i in range(8):
-    xn_3.append(round((x_3[i] - x0_3)/dx_3, 2))
+    if Y[i] - Y_et < dY and Y[i] - Y_et > 0:
+        dY = Y[i] - Y_et
+        number = i
 
-print("a_0 = ", a_0, ", a_1 = ", a_1, ", a_2 = ", a_2, ", a_3 = ", a_3, "\n")
-print("№  |  X1 |  X2  |  X3  |  Y  |  Xn_1  |  Xn_2  |  Xn_3")
-for i in range(8):
-    print(i, " | ", x_1[i], " | ", x_2[i], " | ", x_3[i], " | ", Y[i], " | ", xn_1[i], " | ", xn_2[i], " | ", xn_3[i])
+# функції відгуку, отримана підставленням замість Хi
+# значення факторів ту точку, що задовольняє критерію вибору
+Y2 = a0 + a1 * X[number, 0] + a2 * X[number, 1] + a3 * X[number, 2]
 
-print("\n", "X0 | ", x0_1, " | ", x0_2, " | ", x0_3, "\n", "dx | ", dx_1, " | ", dx_2, " | ", dx_3, "\n")
-
-mid_Y = sum(Y)/len(Y)
-print(mid_Y)
-less_Y = []
-for i in Y:
-    if i < mid_Y:
-        less_Y.append(i)
-print(max(less_Y))
+print("X:\n", X)
+print("Y:\n", Y)
+print("X0: \n", X0)
+print("T_et = ", Y_et)
+print("XNormalized: \n", XNormalized.round(4))
+print("number = ", number)
